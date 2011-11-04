6d583e86d5bb07b458c36ba579e271443fb35e5e
Jonas Mosbech <jonas@mosbech.net>
2011-11-04 22:06:28 +0100

    license info added

--- js/jquery.stickytableheaders.js
+++ js/jquery.stickytableheaders.js
@@ -1,3 +1,6 @@
+/*! Copyright (c) 2011 by Jonas Mosbech - https://github.com/jmosbech/StickyTableHeaders 
+    MIT license info: https://github.com/jmosbech/StickyTableHeaders/blob/master/license.txt */
+
 (function ($) {
 	$.StickyTableHeaders = function (el, options) {
 		// To avoid scope issues, use 'base' instead of 'this'
