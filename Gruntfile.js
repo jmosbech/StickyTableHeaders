8682acbb807d4faf693c3dd1aa926c28b6e02b03
Jonas Mosbech <jonas@mosbech.net>
2014-05-27 19:59:06 +0200

    v0.1.10

--- Gruntfile.js
+++ Gruntfile.js
@@ -4,7 +4,7 @@ module.exports = function(grunt) {
   // Project configuration.
   grunt.initConfig({
     meta: {
-      version: '0.1.9',
+      version: '0.1.10',
       source: 'jquery.stickytableheaders.js',
       sourceMin: 'jquery.stickytableheaders.min.js'
     },
